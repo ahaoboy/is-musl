bb9a7852ee1dfed954abc78f837088dfb805e783
ahaoboy <504595380@qq.com>
2025-10-15 18:17:40 +0800

    check sh

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 rust version of
 https://github.com/napi-rs/napi-rs/blob/b818da29e19db2c633d4e27b23f3b486142ad7bd/cli/src/api/templates/js-binding.ts#L58
 
-## which-shell
+## is-musl
 
 ```shell
 cargo binstall is-musl
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,27 @@
 pub fn is_musl() -> bool {
-  if std::env::consts::OS == "linux" {
-      if is_musl_from_filesystem() == Some(true) {
-          return true;
-      }
-      if is_musl_from_child_process() == Some(true) {
-          return true;
-      }
-  }
+    if std::env::consts::OS == "linux" {
+        if is_musl_from_filesystem() == Some(true) {
+            return true;
+        }
+        if is_musl_from_child_process() == Some(true) {
+            return true;
+        }
+    }
 
-  false
+    false
 }
 
-
 fn is_musl_from_filesystem() -> Option<bool> {
-  std::fs::read_to_string("/usr/bin/ldd")
-      .map(|i| i.contains("musl"))
-      .ok()
+    for i in ["/proc/self/exe", "/bin/sh", "/usr/bin/ldd"] {
+        if let Ok(found) = std::fs::read(i).map(|i| String::from_utf8_lossy(&i).contains("musl")) {
+            return Some(found);
+        }
+    }
+    None
 }
 
 fn is_musl_from_child_process() -> Option<bool> {
-  let s = std::process::Command::new("ldd").arg("--version").output();
-  s.ok()
-      .and_then(|i| String::from_utf8(i.stdout).ok().map(|s| s.contains("musl")))
+    let s = std::process::Command::new("ldd").arg("--version").output();
+    s.ok()
+        .and_then(|i| String::from_utf8(i.stdout).ok().map(|s| s.contains("musl")))
 }
